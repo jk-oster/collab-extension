dbc3321305418a039440c05d6f4e7534c39b29ec
Jakob Osterberger <s2010456022@students.fh-hagenberg.at>
2023-04-05 00:27:21 +0200

    [TASK] added tiptap editor

--- src/components/Messages.vue
+++ src/components/Messages.vue
@@ -1,90 +1,139 @@
 <template>
-  <div class="col-ex-message-container">
-    <div v-for="message in messages" :key="message.id" :id="'msg-' + message.id" class="col-ex-message">
-      <div v-html="wrapUrls(message.text)"></div>
-      <div class="col-ex-message-meta"><span>{{ message.user }}</span><span>{{ (new Date(message.date)).toLocaleDateString() }}</span></div>
+    <div class="col-ex-messages">
+
+        <div class="col-ex-message-container">
+            <div v-for="message in messages" :key="message.id" :id="'msg-' + message.id" class="col-ex-message">
+                <div v-html="wrapUrls(message.text)"></div>
+                <div class="col-ex-message-meta"><span>{{
+                    message.user
+                    }}</span><span>{{ (new Date(message.date)).toLocaleDateString() }}</span></div>
+            </div>
+        </div>
+        <div class="col-ex-message-input">
+            <label style="display: none;" for="newMessage">New message</label>
+            <editor id="newMessage" class="col-ex-editor" v-model="newMessage"/>
+            <!--        <input type="text" id="newMessage" v-model="newMessage" @keyup.enter="addMessage"/>-->
+            <button @click="addMessage" class="col-ex-btn">Send</button>
+        </div>
     </div>
-  </div>
-  <div class="col-ex-message-input">
-    <label style="display: none;" for="newMessage">New message</label>
-    <input type="text" id="newMessage" v-model="newMessage" @keyup.enter="addMessage"/>
-    <button @click="addMessage" class="col-ex-btn">Send</button>
-  </div>
 </template>
 
 <script>
 import {store} from "@/store";
 import {addMessageToSession, getRandId} from "@/firebase";
+import Editor from "@/components/Editor.vue";
 
 export default {
-  name: "Messages",
-  data() {
-    return {
-      newMessage: "",
-    };
-  },
-
-  computed: {
-    messages() {
-      return store.messages.sort((a, b) => a.date - b.date);
-    },
-  },
-
-  methods: {
-    addMessage() {
-      if (this.newMessage) {
-        const messageId = getRandId();
-        addMessageToSession({
-          id: messageId ?? "",
-          user: store.name ?? "",
-          text: this.newMessage ?? "",
-          date: new Date().getTime() ?? 0,
-        }, messageId, store.sessionId);
-        this.newMessage = "";
-      }
+    name: "Messages",
+    components: {Editor},
+    data() {
+        return {
+            newMessage: "",
+        };
     },
 
-    wrapUrls(text) {
-      return text.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" class="col-ex-link" target="_blank">$1</a>');
+    computed: {
+        messages() {
+            return store.messages.sort((a, b) => a.date - b.date);
+        },
     },
-  },
+
+    methods: {
+        addMessage() {
+            if (this.newMessage && this.newMessage.length > 0 && this.newMessage !== '<p></p>') {
+                const messageId = getRandId();
+                addMessageToSession({
+                    id: messageId ?? "",
+                    user: store.name ?? "",
+                    text: this.newMessage ?? "",
+                    date: new Date().getTime() ?? 0,
+                }, messageId, store.sessionId);
+                this.newMessage = "";
+            }
+        },
+
+        wrapUrls(text) {
+            return text.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" class="col-ex-link" target="_blank">$1</a>');
+        },
+    },
 }
 </script>
 
-<style>
+<style lang="scss">
 
-.col-ex-message-container {
-  overflow-y: scroll;
-  max-height: 400px;
+.col-ex {
+  &-message-container {
+    overflow-y: scroll;
+    max-height: 400px;
+    margin-top: 5px;
+  }
+
+&-messages {
+    p, ul, ol, h1, h2, h3, h4, h5, code, pre {
+        margin-top: 0.25em;
+        margin-bottom: 0.25em;
+    }
+    li {
+        margin-top: 0.05em;
+        margin-bottom: 0.05em;
+    }
+  }
+
+  &-message {
+    border: 1px solid #ccc;
+    margin-right: 5px;
+    margin-bottom: 5px;
+    padding: 5px;
+    border-radius: 5px;
+
+    &-meta {
+      display: flex;
+      justify-content: space-between;
+      margin: 0 5px;
+      font-size: 0.5em;
+    }
+
+    &-input > input {
+      width: 75%;
+    }
+  }
+
+  &-link {
+    color: #009dff;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
+  &-editor {
+    margin-top: 0;
+    margin-bottom: 0;
+    min-height: 1em;
+    background-color: #fff;
+    width: 100%;
+  }
+
 }
 
-.col-ex-message {
-  border: 1px solid black;
-  margin: 5px;
-  padding: 5px;
+.ProseMirror {
+  min-height: 1em;
+  width: 100%;
+  background-color: #fff;
+  color: #000;
+  border: none;
+  outline: none;
+  padding: 0 5px;
+
+  > * + * {
+    margin-top: 0.75em;
+  }
+
+  code {
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+  }
 }
 
-.col-ex-message-input {
-  margin: 5px;
-  min-width: 100px;
-  display: flex;
-  justify-content: space-between;
-}
-
-.col-ex-message-input > input {
-  width: 75%;
-}
-
-.col-ex-message > .col-ex-message-meta {
-  display: flex;
-  justify-content: space-between;
-  margin: 0 5px;
-  font-size: 0.5em;
-}
-
-.col-ex-link {
-  color: blue;
-  text-decoration: underline;
-}
 
 </style>
--- src/components/UserScrollFollow.vue
+++ src/components/UserScrollFollow.vue
@@ -1,17 +1,27 @@
 <template>
-  <template v-for="user in users">
-    <div>
-      <button v-if="onSameUrl(user.url)" class="col-ex-btn" @click="scrollTo(user)">
-        Scroll to {{ user.name }}
-      </button>
-      <button v-if="onSameUrl(user.url)" class="col-ex-btn" @click="toggleFollow(user)">
-        Follow {{ user.name }}
-      </button>
-      <a v-if="!onSameUrl(user.url)" class="col-ex-btn" :href="user.url" target="_blank">
-        Open {{ user.name }}'s page
-      </a>
+    <div class="col-ex-scroll-to-container">
+        <div v-if="users.length === 0">Invite other users to join you!</div>
+        <template v-for="user in users">
+            <div class="col-ex-avatar-container">
+            <span>
+                <span class="col-ex-avatar" :style="'background-color: ' + user.color">{{
+                    getInitialLetter(user.name)
+                    }}</span>
+            </span>
+
+                <button v-if="onSameUrl(user.url)" class="col-ex-btn" @click="scrollTo(user)">
+                    Scroll to {{ user.name }}
+                </button>
+                <button v-if="onSameUrl(user.url)" class="col-ex-btn" @click="toggleFollow(user)">
+                   {{ user.id === userToFollow ? 'Unfollow ' : 'Follow ' }}{{ user.name }}
+                </button>
+                <a v-if="!onSameUrl(user.url)" class="col-ex-btn" :href="user.url" target="_blank">
+                    Open {{ user.name }}'s page
+                </a>
+            </div>
+        </template>
     </div>
-  </template>
+
 </template>
 
 <script>
@@ -19,29 +29,52 @@ import {store} from "@/store";
 import {scrollToUser} from "@/service";
 
 export default {
-  name: "UserScrollFollow",
-  props: ['users'],
-  methods: {
-    // Scroll window to center of the position of defined user
-    scrollTo(user) {
-      scrollToUser(user);
+    name: "UserScrollFollow",
+    props: ['users'],
+    computed: {
+        userToFollow() {
+            return store.userToFollow;
+        },
     },
+    methods: {
+        // Scroll window to center of the position of defined user
+        scrollTo(user) {
+            scrollToUser(user);
+        },
 
-    toggleFollow(user) {
-      if (store.userToFollow === user.id) {
-        store.userToFollow = '';
-      } else {
-        store.userToFollow = user.id;
-      }
-    },
+        getInitialLetter(name) {
+            return name.slice(0, 1);
+        },
 
-    onSameUrl(url) {
-      return url === window.location.href;
-    },
-  }
+        toggleFollow(user) {
+            if (store.userToFollow === user.id) {
+                store.userToFollow = '';
+            } else {
+                store.userToFollow = user.id;
+            }
+        },
+
+        onSameUrl(url) {
+            return url === window.location.href;
+        },
+    }
 }
 </script>
 
 <style scoped>
+
+.col-ex-avatar-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.col-ex-scroll-to-container {
+    border: 1px solid #ccc;
+    padding: 5px;
+    border-radius: 5px;
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
 
 </style>
